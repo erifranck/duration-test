e8fbed09565a4a03955b18935a4e16b13814ed37
erifranck <erifranjose@hotmail.com>
2020-02-05 17:08:01 -0500

    create util function to format the value

---
+++ src/utils/timeConverter.ts
@@ -0,0 +1,17 @@
+const concatNullValue = (characters: number) => (value: string) => {
+      return Array.from({length: characters - value.length})
+            .map(() => 0).join('') + value;
+}
+
+export function formatTime(milliseconds: number): string {
+      const ms = concatNullValue(3)(
+            String(milliseconds % 60000)
+      );
+      const seconds = concatNullValue(2)(
+            String(Math.floor(milliseconds / 1000) % 60)
+      );
+      const minutes = concatNullValue(2)(
+            String(Math.floor(milliseconds / 60000))
+      );
+      return `${minutes}:${seconds}:${ms}`;
+};
