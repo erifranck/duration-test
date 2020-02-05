850735492174189a9ed6c9887dc7bfbb769da149
erifranck <erifranjose@hotmail.com>
2020-02-05 18:03:03 -0500

    validate non negative values

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface Props {
       onChange: (value: string) => void;
 }
+interface State {
+      value: string
+}
 
 export const Input = (props: Props) => {
+      const [state, setState] = useState<State>({value: '0'})
+
+      const onChange = (ev:React.ChangeEvent<HTMLInputElement>) => {
+            const value = ev.currentTarget.value;
+            if(Number(value) >= 0) {
+                  setState({value});
+                  props.onChange( value || '0')
+            }
+      }
       return (
-            <input type="number"
-             onChange={ (ev) => props.onChange(ev.currentTarget.value || '0')}/>
+            <input type="number" value={state.value} onChange={onChange}/>
       );
 }
